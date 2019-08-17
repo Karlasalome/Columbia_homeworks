90b31a14d757600d12273bf81e68b7180e581293
Karla <prattskarla@gmail.com>
2019-08-17 17:39:39 -0400

    JavaScript Code Formatting

--- JavaScript/StarterCode/static/js/app.js
+++ JavaScript/StarterCode/static/js/app.js
@@ -9,10 +9,11 @@ var resetbtn = d3.select("#reset-btn");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 var populate = (dataInput) => {
-      dataInput.forEach(ufo_sightings => {
-var row = tbody.append("tr");
-       columns.forEach(column => row.append("td").text(ufo_sightings[column])
-   )
+
+  dataInput.forEach(ufo_sightings => {
+
+    var row = tbody.append("tr");
+  columns.forEach(column => row.append("td").text(ufo_sightings[column]))
  });
 }
 
@@ -22,11 +23,13 @@ populate(data);
 // Filter by attribute
 button.on("click", () => {
     d3.event.preventDefault();
-var inputDate = inputField1.property("value").trim();
+
+    var inputDate = inputField1.property("value").trim();
  // Filter by field matching input value
 var filterDate = data.filter(data => data.datetime === inputDate);
     console.log(filterDate);
  // Add filtered sighting to table
+
  tbody.html("");
 let response = {
  //  filterData, filterCity, filterDate
