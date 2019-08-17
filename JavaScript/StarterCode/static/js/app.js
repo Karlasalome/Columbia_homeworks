44b1d00966c2ec0a2ba1af82b69d63d019fe50b2
Karla <prattskarla@gmail.com>
2019-08-17 17:32:00 -0400

    clean up code

--- JavaScript/StarterCode/static/js/app.js
+++ JavaScript/StarterCode/static/js/app.js
@@ -1,9 +1,6 @@
 // from data.js
 //var tableData = data;
 
-// YOUR CODE HERE!
-
-// Variables
 var button = d3.select("#filter-btn");
 var inputField1 = d3.select("#datetime");
 var inputField2 = d3.select("#city");
@@ -12,10 +9,9 @@ var resetbtn = d3.select("#reset-btn");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 var populate = (dataInput) => {
-
- dataInput.forEach(ufo_sightings => {
-   var row = tbody.append("tr");
-   columns.forEach(column => row.append("td").text(ufo_sightings[column])
+      dataInput.forEach(ufo_sightings => {
+var row = tbody.append("tr");
+       columns.forEach(column => row.append("td").text(ufo_sightings[column])
    )
  });
 }
@@ -25,51 +21,31 @@ populate(data);
 
 // Filter by attribute
 button.on("click", () => {
- d3.event.preventDefault();
- var inputDate = inputField1.property("value").trim();
- // var inputCity = inputField2.property("value").toLowerCase().trim();
+    d3.event.preventDefault();
+var inputDate = inputField1.property("value").trim();
  // Filter by field matching input value
- var filterDate = data.filter(data => data.datetime === inputDate);
- console.log(filterDate)
- //var filterCity = data.filter(data => data.city === inputCity);
- //console.log(filterCity)
- //var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
- //console.log(filterData)
-
+var filterDate = data.filter(data => data.datetime === inputDate);
+    console.log(filterDate);
  // Add filtered sighting to table
  tbody.html("");
-
- let response = {
+let response = {
  //  filterData, filterCity, filterDate
- filterDate
+    filterDate
  }
-
- /*
- if (response.filterData.length !== 0) {
-   populate(filterData);
- }
-   else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
-     populate(filterCity) || populate(filterDate);
-
-   }
-   else {
-     tbody.append("tr").append("td").text("No results found!");
-   }
-})
-*/
 
 if (response.filterDate.length !== 0) {
- populate(filterDate);
+    populate(filterDate);
 }
  else if (response.filterDate.length === 0 ) {
    populate(filterDate);
  }
- else {
+  else {
    tbody.append("tr").append("td").text("No results found!");
  }
 
 resetbtn.on("click", () => {
- tbody.html("");
- populate(data);
- console.log("Table reset");
+tbody.html("");
+populate(data);
+
+console.log("Table reset");
 })});
